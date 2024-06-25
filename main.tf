1971c332dc662ce968408fe3316b03a5e0faba52
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-25 18:15:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired region
+  region = "us-east-2"  # Replace with your desired region
 }
 
 resource "aws_instance" "mongo" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a suitable Amazon Linux 2 AMI
+  ami           = "ami-0edf386e462400a51"  # Replace with a suitable Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  key_name      = "your-key-pair"  # Replace with your key pair
+  key_name      = "project"  # Replace with your key pair
 
   tags = {
     Name = "MongoDBServer"
