26516979f40d29ed563907211179a7b9d7c8962d
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-26 23:03:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "mongodb" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file(var.private_key_path)
+      private_key = file("${path.module}/keys/my-key.pem")
       host        = self.public_ip
     }
   }
