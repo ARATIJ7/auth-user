e55a13f08ab67c586f660dfba5fcc64d738c60ef
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-27 00:29:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "mongodb" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  key_name      = var.key_name
+  
   user_data = file("${path.module}/user_data.sh")
 
   tags = {
