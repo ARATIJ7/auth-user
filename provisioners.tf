29004acabe24baf1127fa84b73cf521db0d36090
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-26 22:21:31 +0530

    Create provisioners.tf

---
+++ provisioners.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "mongo_user_setup" {
+  depends_on = [aws_instance.mongodb]
+
+  provisioner "remote-exec" {
+    inline = [
+      "mongo --eval 'db.createUser({user:\"admin\",pwd:\"password123\",roles:[{role:\"userAdminAnyDatabase\",db:\"admin\"}]})'",
+      "sudo sed -i '/#security:/a\\security:\\n  authorization: \"enabled\"' /etc/mongod.conf",
+      "sudo systemctl restart mongod"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.private_key_path)
+      host        = aws_instance.mongodb.public_ip
+    }
+  }
+}
