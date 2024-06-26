6289d85fcbeda83416691d165fedf1713a49949b
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-27 00:32:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,9 @@ variable "instance_type" {
   description = "The instance type to use"
   default     = "t2.micro"
 }
+
+variable "key_name" {
+  description = "The key name to use"
+  default     = "project"
+}
+
