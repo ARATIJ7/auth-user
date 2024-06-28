310c7070d40984ebea57b7d527fa00cae2b21a41
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-28 19:54:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,30 @@
-variable "aws_region" {
-  description = "The AWS region to deploy to"
+variable "region" {
+  description = "The AWS region to create resources in."
   default     = "us-east-2"
 }
 
-variable "ami_id" {
-  description = "The AMI ID to use for the instance"
-  default     = "ami-024ebc7de0fc64e44" # Example Amazon Linux 2 AMI
+variable "key_name" {
+  description = "The name of the SSH key pair."
+  default     = "terraform-key"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key to use for SSH access."
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "ami" {
+  description = "The AMI ID to use for the instance."
+  default     = "ami-024ebc7de0fc64e44"  # Amazon Linux 2 AMI (change as needed)
 }
 
 variable "instance_type" {
-  description = "The instance type to use"
+  description = "The type of instance to use."
   default     = "t2.micro"
 }
 
-variable "key_name" {
-  description = "The key name to use"
-  default     = "project"
+variable "allowed_cidrs" {
+  description = "List of CIDR blocks allowed to access MongoDB."
+  type        = list(string)
+  default     = ["0.0.0.0/0"]  # Open to the world, change as needed for security
 }
-
